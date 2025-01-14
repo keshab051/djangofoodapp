65a42b9fc7a1c18aae93d433263626bf9e79cd38
KeshabNeupane <keshabneupane061@gmail.com>
2025-01-14 07:30:06 +0545

    setting up the default profile picture

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,12 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
-    
-
 from django.contrib import admin
 from django.urls import include,path
 from users import views as user_views
 from django.contrib.auth import views as auth_views
+from django.conf import settings
+from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('food.urls')),
@@ -30,3 +30,8 @@ urlpatterns = [
     path('profile/',user_views.profilepage, name='profile'),
    
 ]
+
+urlpatterns += [
+    # ... the rest of your URLconf goes here ...
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    

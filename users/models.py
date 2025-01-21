2f82f291310cecd4bd1ccd86240f025b002477c1
KeshabNeupane <keshabneupane061@gmail.com>
2025-01-21 23:05:45 +0545

    fixing problems in image fetching process

--- users/models.py
+++ users/models.py
@@ -4,7 +4,8 @@ from django.contrib.auth.models import User       #   importing the existing mod
 
 class Profile(models.Model):
     user = models.OneToOneField(User,on_delete=models.CASCADE)
-    image = models.ImageField(default='profilepic.jpg',upload_to='profile_pictures')
+    # for image field first you have to install a new package called pillow
+    image = models.ImageField(upload_to='profile_pictures',default='profilepic.jpg')
     location = models.CharField(max_length=100)
 
     def __str__(self):
